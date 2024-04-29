6768ae566f1bd9c57e996ad079399e324679c7f2
jean-renoul <jean.renoul@laplateforme.io>
2024-04-29 17:22:31 +0200

    jobs finis jusqu'au 3

---
+++ jour01/job01/main.cpp
@@ -0,0 +1,11 @@
+x = p + 3
+-> short int 13
+
+y = c + 1
+-> int 2
+
+z = p + c
+-> int 11
+
+w = 3 * p + 5 * c
+-> int 35
---
+++ jour01/job03/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n = 5;
+    while (n != 0) {
+        cout << "Hello World !\n";
+        n--;
+    }
+    return 0;
+}
